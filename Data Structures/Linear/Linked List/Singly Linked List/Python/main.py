e29efe1ec9e704fddf1f335392e26cac63ade4e7
KishanBGajera <kishangajera1411@gmail.com>
2024-01-08 21:04:42 +0530

    Updated main.py

--- Data Structures/Linear/Linked List/Singly Linked List/Python/main.py
+++ Data Structures/Linear/Linked List/Singly Linked List/Python/main.py
@@ -1,4 +1,16 @@
 from linked_list import Node, LinkedList
 
-ll = LinkedList(10)
-ll.add_to_linked_list(Node(10))
+def main():
+    ll = LinkedList(10)
+    ll.add_to_linked_list(Node(11))
+    ll.add_to_linked_list(Node(12))
+    
+    ll.display_linked_list()
+
+    ll.pop_out_of_linked_list()
+    ll.add_to_linked_list(Node(13))
+
+    ll.display_linked_list()
+
+if __name__ == "__main__":
+    main()
