64f6a76d2325eecf39854baf92a5a6cdfc61c99e
KishanBGajera <kishangajera1411@gmail.com>
2024-01-08 21:55:20 +0530

    Added Quick Sort

---
+++ Algorithm/Sorting/Quick Sort/quickSort.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "../array.h"
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main() {
+    int arr[] = {9, 7, 3, 6, 1, 7, 8, 2, 3};
+    int n = sizeof(arr) / sizeof(int);
+
+    printf("Original array: ");
+    printArray(arr, n);
+
+    quickSort(arr, 0, n - 1);
+
+    printf("Sorted array: ");
+    printArray(arr, n);
+
+    return 0;
+}
