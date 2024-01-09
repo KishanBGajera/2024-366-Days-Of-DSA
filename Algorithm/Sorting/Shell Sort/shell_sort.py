487f000de7fc388612e2d2c5f4f3876a035f5159
KishanBGajera <kishangajera1411@gmail.com>
2024-01-09 21:19:12 +0530

    Added shell sort

---
+++ Algorithm/Sorting/Shell Sort/shell_sort.py
@@ -0,0 +1,23 @@
+def shell_sort(arr):
+    n = len(arr)
+    gap = n // 2
+
+    while gap > 0:
+        for i in range(gap, n):
+            temp = arr[i]
+            j = i
+
+            while j >= gap and arr[j - gap] > temp:
+                arr[j] = arr[j - gap]
+                j -= gap
+
+            arr[j] = temp
+
+        gap //= 2
+
+arr = [12, 34, 54, 2, 3]
+print("Original array:", arr)
+
+shell_sort(arr)
+
+print("Array after Shell Sort:", arr)
